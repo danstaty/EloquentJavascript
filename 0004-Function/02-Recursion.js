6f1d4f36ab95381e41af804ab8ef2dfbd8c5d000
gorbunovia <work.gia.gmc@gmail.com>
2015-03-25 15:27:05 +0300

    Add my solution
    add my solution

--- 0004-Function/02-Recursion.js
+++ 0004-Function/02-Recursion.js
@@ -1,1 +1,16 @@
+// My solution
+function isEven(num){
+  while(num > 1)
+  num -= 2;
+  if(num == 1)
+  return false;
+  else if(num == 0)
+  return true;
+};
 
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
