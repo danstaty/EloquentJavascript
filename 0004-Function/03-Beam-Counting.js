67ecfd5ea105b0e4ae0606a37d2e88c075ea8b45
gorbunovia <work.gia.gmc@gmail.com>
2015-03-25 16:34:00 +0300

    Corrected my Solution
    Corrected my Solution. It's the same to book's solution.

--- 0004-Function/03-Beam-Counting.js
+++ 0004-Function/03-Beam-Counting.js
@@ -1,18 +1,14 @@
 // My solution
-function countBs(name){
-  var count = 0;
-  for(i=0; i<name.length; i++)
-  if(name.charAt(i) === "B")
-  count += 1;
-  return count;
-};
-
 function countChar(name, char){
     var count = 0;
   for(i=0; i<name.length; i++)
   if(name.charAt(i) === char)
   count += 1;
   return count;
+};
+
+function countBs(name){
+  return countChar(name, "B")
 };
 
 console.log(countBs("BBC"));
