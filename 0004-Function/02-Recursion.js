c0874efdb1f5486f5a319ebe996c843b447de6a0
gorbunovia <work.gia.gmc@gmail.com>
2015-03-25 15:59:38 +0300

    Add book solution
    Add book solution

--- 0004-Function/02-Recursion.js
+++ 0004-Function/02-Recursion.js
@@ -6,7 +6,9 @@ function isEven(num){
   return false;
   else if(num == 0)
   return true;
-};
+  else if(num < 0)
+  return isEven(-num);
+  };
 
 console.log(isEven(50));
 // → true
@@ -14,3 +16,23 @@ console.log(isEven(75));
 // → false
 console.log(isEven(-1));
 // → ??
+
+// Book Solution
+function isEven(n) {
+  if (n == 0)
+    return true;
+  else if (n == 1)
+    return false;
+  else if (n < 0)
+    return isEven(-n);
+  else
+    return isEven(n - 2);
+}
+
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → false
