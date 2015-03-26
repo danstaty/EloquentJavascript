f776c26959027ae381a1e1ae2c188831797fd21b
gorbunovia <work.gia.gmc@gmail.com>
2015-03-26 17:27:34 +0300

    Stage of find to correlation

--- 0005-Data-Structures/00-Data-Structures.js
+++ 0005-Data-Structures/00-Data-Structures.js
@@ -1,3 +1,46 @@
 /**
  * Created by davey on 26.03.2015.
  */
+
+
+// Добавляем данные в журнал
+
+var Journal = [];
+
+function addEntry(events, didTurnIntoSquirrel){
+    Journal.push(
+        {
+            event: events,
+            squirrel: didTurnIntoSquirrel
+        }
+    );
+};
+
+//Описываем функцию получения корреляции
+
+function phi(table){
+
+    return (table[3] * table[0] - table[2] * table[1]) /
+        Math.sqrt((table[2] + table[3]) *
+        (table[0] + table[1]) *
+        (table[1] + table[3]) *
+        (table[0] + table[2]));
+
+console.log(phi([76, 9, 4, 1]));
+// → 0.068599434
+
+
+//Находим корреляцию для конкертного события
+
+    function hasEvent(){
+
+
+    };
+
+    function tableFor(events, array){
+
+    };
+};
+
+console.log(tableFor("pizza", JOURNAL));
+// → [76, 9, 4, 1]
