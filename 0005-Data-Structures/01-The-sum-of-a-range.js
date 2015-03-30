176378b0c1bc33ff1c7d7dc0be0fee864d811017
gorbunovia <work.gia.gmc@gmail.com>
2015-03-30 17:59:50 +0300

    First part of solution 01-The-sum-of-a-range

--- 0005-Data-Structures/01-The-sum-of-a-range.js
+++ 0005-Data-Structures/01-The-sum-of-a-range.js
@@ -1,3 +1,27 @@
 /**
  * Created by davey on 30.03.2015.
  */
+function range(start, fin) {
+    var arrayMe = [];
+    for(var i = start; i <= fin; i++){
+        arrayMe.push(i);
+    };
+    return arrayMe;
+};
+
+function sum(array){
+    var summer = 0;
+    for(var i = 0; i < array.length; i++){
+       summer = summer + array[i];
+    };
+    return summer;
+};
+
+
+
+console.log(sum(range(1, 10)));
+// → 55
+
+
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
