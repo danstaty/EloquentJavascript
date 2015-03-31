42c290ecc8be875a30f091fd0f5b08d43a806937
gorbunovia <work.gia.gmc@gmail.com>
2015-03-31 16:23:45 +0300

    Second solution

--- 0005-Data-Structures/03-List.js
+++ 0005-Data-Structures/03-List.js
@@ -24,12 +24,28 @@ function arrayToList(array){
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 
+//Second Solution
+function arrayToList(array) {
+    var list = null;
+    for (var i = array.length - 1; i >= 0; i--)
+        list = {value: array[i], rest: list};
+    return list;
+}
 
-
-
-
+function listToArray(list) {
+    var array = [];
+    for (var node = list; node; node = node.rest)
+        array.push(node.value);
+    return array;
+}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
+
+//Third Solution
+
+
+
+
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(nth(arrayToList([10, 20, 30]), 1));
