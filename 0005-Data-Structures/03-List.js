b905b930a9ab49467aa447aa40c708a3e6260810
gorbunovia <work.gia.gmc@gmail.com>
2015-03-31 17:12:35 +0300

    Forth Solution

--- 0005-Data-Structures/03-List.js
+++ 0005-Data-Structures/03-List.js
@@ -50,8 +50,13 @@ console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
 
 //Fourth Solution
-
-
-
+function nth(list, position){
+    if(!list)
+    return undefined;
+    else if(position == 0)
+    return list.value;
+    else
+    return nth(list.rest, n-1)
+};
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
