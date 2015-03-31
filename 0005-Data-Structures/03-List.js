52c055c62ae0267563d05edccc90db65bb4f4c50
gorbunovia <work.gia.gmc@gmail.com>
2015-03-31 16:52:19 +0300

    Third Solution

--- 0005-Data-Structures/03-List.js
+++ 0005-Data-Structures/03-List.js
@@ -42,11 +42,16 @@ console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 
 //Third Solution
-
-
-
+function prepend(add, list){
+    return {value: add, rest: list}
+};
 
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
+
+//Fourth Solution
+
+
+
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
