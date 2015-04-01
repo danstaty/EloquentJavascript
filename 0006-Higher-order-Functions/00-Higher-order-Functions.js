db7562d0a4238bc0491ceb45183ff92a4730f598
gorbunovia <work.gia.gmc@gmail.com>
2015-04-01 17:38:48 +0300

    Started work with JSON

--- 0006-Higher-order-Functions/00-Higher-order-Functions.js
+++ 0006-Higher-order-Functions/00-Higher-order-Functions.js
@@ -1,3 +1,147 @@
 /**
  * Created by davey on 01.04.2015.
  */
+//Version 1
+var array = [1, 2, 3];
+for (var i = 0; i < array.length; i++) {
+    var current = array[i];
+    console.log(current);
+}
+
+//Verison 2
+function logEach(array) {
+    for (var i = 0; i < array.length; i++)
+        console.log(array[i]);
+}
+
+//Version 3
+function forEach(array, action) {
+    for (var i = 0; i < array.length; i++)
+        action(array[i]);
+}
+
+forEach(["Тили", "Мили", "Трямдия"], console.log);
+// → Тили
+// → Мили
+// → Трямдия
+
+//Calculation sum numbers of array.
+var numbers = [1, 2, 3, 4, 5], sum = 0;
+rofHcae(numbers, function(number) {
+    sum += number;
+});
+console.log(sum);
+// → 15
+
+
+//Conversion code from chapter 4 - "Final analysis"
+//---> old version
+function gatherCorrelations(journal) {
+    var phis = {};
+    for(var entry = 0; entry < journal.length; entry++){
+        var events = journal[entry].events;
+        for(var i = 0; i < events.length; i++){
+            var event = events[i];
+            if(!(event in phis))
+            phis[event] = phi(tableFor(entry,journal));
+        }
+        return phis;
+    }
+}
+
+
+//---> new version with array method forEach
+function gatherCorrelations(journal) {
+        var phis = {};
+        journal.forEach(function(entry){
+            entry.events.forEach(function(event){
+                if(!(event in phis))
+                phis[event] = phi(tableFor(entry,journal))
+            })
+        })
+    return phis;
+}
+
+
+//Create new function
+function greaterThan(n) {
+    return function(m) { return m > n; };
+}
+var greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
+// → true
+
+//Function change another functions
+function noisy(f) {
+    return function(arg){
+        console.log("calling with " + arg)
+        var val = f(arg)
+        console.log("called with " + arg + " - got " + val)
+    };
+
+
+}
+noisy(Boolean)(0);
+// → calling with 0
+// → called with 0 - got false
+
+
+//Create New Streams
+function unless(test, then) {
+    if (!test) then();
+}
+function repeat(times, body) {
+    for (var i = 0; i < times; i++) body(i);
+}
+
+repeat(3, function(n) {
+    unless(n % 2, function() {
+        console.log(n, "is even");
+    });
+});
+// → 0 is even
+// → 2 is even
+
+
+
+
+//Work with JSON
+
+var string = JSON.stringify({name: "X", born: 1980});
+console.log(string);
+// → {"name":"X","born":1980}
+console.log(JSON.parse(string).born);
+// → 1980
+
+
+//Work with list ancestors
+var ancestry = JSON.parse(ANCESTRY_FILE);
+console.log(ancestry.length);
+// → 39
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
