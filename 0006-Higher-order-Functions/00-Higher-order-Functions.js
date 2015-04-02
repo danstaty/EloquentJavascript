6ef0c331b6f68fe3db4669e45dc0358307cae475
gorbunovia <work.gia.gmc@gmail.com>
2015-04-02 17:00:16 +0300

    Working on Composability

--- 0006-Higher-order-Functions/00-Higher-order-Functions.js
+++ 0006-Higher-order-Functions/00-Higher-order-Functions.js
@@ -211,5 +211,36 @@ console.log(ancestry.reduce(function(min, cur){
 // → {name: "Pauwels van Haverbeke", born: 1535, …}
 
 
+//Composability
+function average(array){
+    function plus(a, b){return a + b;};
+    return array.reduce(plus) / array.length;
+};
+
+function age(p){return p.died - p.born}
+function male(p){return p.sex == "m";}
+function female(p){return p.sex == "f";}
+
+console.log(average(ancestry.filter(male).map(age)));
+// → 61.67
+console.log(average(ancestry.filter(female).map(age)));
+// → 54.56
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
