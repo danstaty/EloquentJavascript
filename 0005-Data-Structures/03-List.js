925908d52b11e08fdb849826ad8a39a6792e3c5a
gorbunovia <work.gia.gmc@gmail.com>
2015-08-11 15:41:35 +0300

    Data Structures is completed

--- 0005-Data-Structures/03-List.js
+++ 0005-Data-Structures/03-List.js
@@ -76,7 +76,7 @@ var list = {
 
 function arrayToList(someArray) {
     var list = null
-    for(var i = 0; i < someArray.length; i++ ){
+    for(var i = someArray.length - 1; i >= 0; i-- ){
         list = {value: someArray[i], rest: list}
     }
     return list
@@ -88,27 +88,65 @@ console.log(arrayToList([10, 20]));
 
 
 function listToArray(someList){
-
+    var newArray = []
+    for(var node = someList; node; node = node.rest){
+        newArray.push(node.value)
+    }
+    return newArray
 }
-
-
 
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 
 
+function prepend(x, y){
+    return list = {value: x, rest: y}
 
+}
 
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
+
+function nth(list, pos) {
+        if(pos === 0)
+            return list.value
+        else
+            return nth(list.rest, pos - 1)
+
+
+        }
+
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
 
 
 
+function deepEqual(a, b) {
+    if(a === b) return true
 
+    if(a == null || typeof a != "object" ||
+        b == null || typeof  b != "object")
+        return false
+    var propsInA = 0;
+    var propsInB = 0;
 
+    for(prop in a)
+        propsInA += 1
+    for(prop in b)
+        propsInB += 1
+        if(!(prop in a) || !deepEqual(a[prop], b[prop]))
+            return false
+    return propsInA == propsInB
 
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
 
 
 
