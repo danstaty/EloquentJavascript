f55155c2a1104bd0d8f750ab982d94ac02bbefcf
gorbunovia <work.gia.gmc@gmail.com>
2015-08-12 11:24:25 +0300

    Started work with JSON

--- 0006-Higher-order-Functions/00-Higher-order-Functions.js
+++ 0006-Higher-order-Functions/00-Higher-order-Functions.js
@@ -29,22 +29,73 @@ function gatherCorrelations(journal) {
 }
 
 
+//function change another function
+function noisy(f){
+    /*Не нужно забывать о принципе наследования. Когда вложенная функция помнит все окружение и может использовать аргу
+    * менты объявленные выше. Таким образом параметр f - принимает значение Boolean, а параметр arg принимает значение 0*/
+    return function(arg){
+             console.log("calling with " + arg)
+        var val = f(arg)
+        console.log("called with " + arg + "-got" + val)
+        return val
+    }
+}
+
+noisy(Boolean)(0);
+/*Это выражение можно написать иначе
+*var temp = noisy(Boolean);
+ temp(0);
+* */
 
 
+// → calling with 0
+// → called with 0 - got false
+
+function unless(test, then) {
+    if (!test) then()
+}
+function repeat(times, body) {
+    for (var i = 0; i < times; i++) body(i)
+}
+
+repeat(3, function(n) {
+    unless(n % 2, function() {
+        console.log(n, "is even");
+    });
+});
+// → 0 is even
+// → 2 is even
 
 
+//Чтение JSON
+
+var ancestry = JSON.parse(ANCESTRY_FILE)
+console.log(ancestry.length)
+
+//Фильруем массив
+
+    function filter(array, testt){
+        var passed = []
+        for(var i = 0; i < array.length; i++)
+            if(testt(array[i]))
+                passed.push(array[i])
+        return passed
+    }
 
 
+console.log(filter(ancestry, function(person){
+    person.born > 1900 && person.born < 1925;
+}))
+// → [{name: "Philibert Haverbeke", …}, …]
 
 
+//используем стандартный метод массивов .filter
 
+    console.log(ancestry.filter(function(person){
+        return person.father == "Carel Haverbeke"
+    }))
 
-
-
-
-
-
-
+//Преобразование при помощи map
 
 
 
