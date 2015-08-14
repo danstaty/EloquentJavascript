4d748298e9010d57c7540da1254879e48f89da06
gorbunovia <work.gia.gmc@gmail.com>
2015-08-14 10:17:12 +0300

    Finished work with base higher order function

--- 0006-Higher-order-Functions/00-Higher-order-Functions.js
+++ 0006-Higher-order-Functions/00-Higher-order-Functions.js
@@ -178,35 +178,80 @@ console.log(average(ancestry.filter(female).map(age)));
 //Строим объект сопостовляющий имена и людей
 var byName = []
 ancestry.forEach(function(person){
-    byName[person.name] = person
- })
+    byName[name.person] = person
+})
+
 
 console.log(byName["Philibert Haverbeke"]);
 // → {name: "Philibert Haverbeke", …}
 
-
 //Подсчет ДНК
 
-function reduceAncestors(person, f, defaultValue) {
-    function valueFor(person) {
-        if (person == null)
+function reduceAncestors(person, f, defaultValue){
+    function valueFor(person){
+        if(person == null)
             return defaultValue;
         else
-            return f(person, valueFor(byName[person.mother]),
-                valueFor(byName[person.father]));
+            return f(person, valueFor(person.mother), valueFor(person.father))
     }
-    return valueFor(person);
+    return valueFor
 }
 
-function sharedDNA(person, fromMother, fromFather) {
-    if (person.name == "Pauwels van Haverbeke")
+function sharedDNA(person, fromMother, fromFather){
+    if(person.name == "Pauwels van Haverbeke")
         return 1;
     else
-        return (fromMother + fromFather) / 2;
+        return (fromFather + fromMother) / 2
 }
+
 var ph = byName["Philibert Haverbeke"];
 console.log(reduceAncestors(ph, sharedDNA, 0) / 4);
 // → 0.00049
+
+// % > 70 years
+function countAncestors(person, test) {
+    function combine(person, fromMother, fromFather) {
+        var thisOneCounts = test(person);
+        return fromMother + fromFather + (thisOneCounts ? 1 : 0);
+    }
+    return reduceAncestors(person, combine, 0);
+}
+function longLivingPercentage(person) {
+    var all = countAncestors(person, function(person) {
+        return true;
+    });
+    var longLiving = countAncestors(person, function(person) {
+        return (person.died - person.born) >= 70;
+    });
+    return longLiving / all;
+}
+console.log(longLivingPercentage(byName["Emile Haverbeke"]));
+// → 0.145
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
