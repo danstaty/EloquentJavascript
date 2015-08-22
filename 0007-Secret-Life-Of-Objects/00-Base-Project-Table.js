5814a61e9631e044661d2674e02363e188de1e90
gorbunovia <work.gia.gmc@gmail.com>
2015-08-22 09:26:45 +0300

    Second version gatherCorrelations

---
+++ 0007-Secret-Life-Of-Objects/00-Base-Project-Table.js
@@ -0,0 +1,22 @@
+/*Первая часть программы вычисляет массивы минимальных ширин колонок и высот строк для матрицы ячеек.
+    Переменная rows будет содержать массив массивов, где каждый внутренний массив – это строка ячеек.*/
+
+function rowHeights(rows) {
+    return rows.map(function(row) {
+        return row.reduce(function(max, cell) {
+            return Math.max(max, cell.minHeight());
+        }, 0);
+    });
+}
+
+function colWidths(rows) {
+    return rows[0].map(function(_, i) {
+        return rows.reduce(function(max, row) {
+            return Math.max(max, row[i].minWidth());
+        }, 0);
+    });
+}
+
+
+
+
